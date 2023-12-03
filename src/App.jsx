9265843fba7cf44b66cf506bc86214cdd5d1b55d
AudunKodehode <audunkodehode@gmail.com>
2023-12-03 01:37:06 +0100

    *

--- src/App.jsx
+++ src/App.jsx
@@ -13,55 +13,65 @@ function App() {
   const [classType, setClassType] = useState("");
   const [name, setName] = useState("");
   const [playerCardVisibility, setPlayerCardVisibility] = useState(true);
-  const [health, setHealth] = useState(100);
+  const [health, setHealth] = useState(40);
   const [gold, setGold] = useState(0);
-  
-  let player = 
-    {
-      name: `${name}`,
-      class: `${classType}`,
-      health: `${health}`,
-      skills: {
-        strength: 0,
-        dexterity: 0,
-        constitution: 0,
-        intelligence: 0,
-        wisdom: 0,
-        charisma: 0
-      },
-      inventory: {
-        gold: `${gold}`,
-        items: [
-          {
-            name: "Beginner Sword",
-            damage: 10,
-            type: "weapon",
-            quantity: 1,
-            durability: 100,
-            description: "This is a sword",
-            itemlevel : 1
-          },
-          {
-            name: "Beginner Shield",
-            defense: 10,
-            type: "armor",
-            quantity: 1,
-            durability: 100,
-            description: "This is a shield",
-            itemlevel : 1
-          },
-          {
-            name: "Potion",
-            heal: 10,
-            type: "consumable",
-            quantity: 1,
-            description: "This is a potion",
-            itemlevel : 1
-          },
-        ]
-      }
-    }
+  const [exp, setExp] = useState(0);
 
+  let player = {
+    name: `${name}`,
+    class: `${classType}`,
+    health: `${health}`,
+    exp: `${exp}`,
+    skills: [
+      {
+        name:"Strength",
+        level: 1,
+      },
+      {
+        name:"Dexterity",
+        level: 1,
+      },
+      {
+        name:"Intelligence",
+        level: 1,
+      },
+      {
+        name:"Endurance",
+        level: 1,
+      }
+    ],
+    inventory: {
+      gold: `${gold}`,
+      items: [
+        {
+          name: "Beginner Sword",
+          damage: 10,
+          type: "weapon",
+          quantity: 1,
+          durability: 100,
+          description: "This is a sword",
+          itemlevel: 1,
+        },
+        {
+          name: "Beginner Shield",
+          defense: 10,
+          type: "armor",
+          quantity: 1,
+          durability: 100,
+          description: "This is a shield",
+          itemlevel: 1,
+        },
+        {
+          name: "Beginner Potion",
+          heal: 10,
+          type: "consumable",
+          quantity: 1,
+          description: "This is a potion",
+          itemlevel: 1,
+        },
+      ],
+    },
+  };
 
   const incrementDays = () => {
     if (day < 7) {
@@ -71,11 +81,19 @@ function App() {
       incrementWeek();
     }
   };
-  
-  const  dayString = (day) => {
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  const dayString = (day) => {
+    const days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
     return days[day - 1];
-  }
+  };
 
   const incrementWeek = () => {
     if (week < 4) {
@@ -95,10 +113,23 @@ function App() {
     }
   };
 
-  const  monthString = (month) => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthString = (month) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
     return months[month - 1];
-  }
+  };
 
   const incrementYear = () => {
     setYear(year + 1);
@@ -107,9 +138,28 @@ function App() {
   return (
     <>
       <main className="grid items-center justify-center min-h-screen grid-cols-12 grid-rows-6 bg-dark">
-        <Calender day={dayString(day)} week={week} month={monthString(month)} year={year} />
-        <DashBoard setPlayerCardVisibility={setPlayerCardVisibility} incrementDays={incrementDays} classType={classType} setClassType={setClassType} name={name} setName={setName}/>
-        <PlayerCard playerCardVisibility={playerCardVisibility} player={player} setHealth={setHealth} health={health}  setGold={setGold} gold />
+        <Calender
+          day={dayString(day)}
+          week={week}
+          month={monthString(month)}
+          year={year}
+        />
+        <DashBoard
+          setPlayerCardVisibility={setPlayerCardVisibility}
+          incrementDays={incrementDays}
+          classType={classType}
+          setClassType={setClassType}
+          name={name}
+          setName={setName}
+        />
+        <PlayerCard
+          playerCardVisibility={playerCardVisibility}
+          player={player}
+          setHealth={setHealth}
+          health={health}
+          setGold={setGold}
+          gold
+        />
       </main>
     </>
   );
--- src/components/Playercard.jsx
+++ src/components/Playercard.jsx
@@ -7,19 +7,30 @@ export const PlayerCard = ({
   setHealth,
   setGold,
 }) => {
-  const [inventoryOpen, setInventoryOpen] = useState(true);
+    const [inventoryOpen, setInventoryOpen] = useState(true);
+    const [statsOpen, setStatsOpen] = useState(true);
+  
   function handleItemClick(item) {
-    item.type === "consumable" ? () => handleConsumeItem(item) : "";
+    if (item.type === "consumable") {
+      let newHealth = health + 10;
+      if (newHealth > 100) {
+        setHealth(100);
+      } else {
+        setHealth(newHealth);
+      }
+    }
+    item.quantity = item.quantity - 1;
+    if (item.quantity === 0) {
+    // remove item
+    }
+    console.log(item.quantity)
   }
 
-  function handleConsumeItem(item) {
-    item.type === "consumable" ? setHealth(health + item.heal) : "";
-    console.log(item);
-  }
+
   return (
     <div>
       {playerCardVisibility === true ? (
-        <div className="fixed bottom-0 left-0 flex-col w-64 h-64 gap-6 text-black bg-gray-200 position-fixed">
+        <div className="fixed bottom-0 left-0 flex-col w-[256px] h-64 gap-6 text-black bg-gray-200 position-fixed ">
           <img
             className="w-24"
             src={`/images/classtypes/${player.class.toLowerCase()}.webp`}
@@ -40,22 +51,28 @@ export const PlayerCard = ({
             Health: {player.health}
           </p>
 
-          <div className="flex justify-around justify-self-end">
+          <div className="flex justify-around m-2 justify-self-end">
             <button
               onClick={() => setInventoryOpen(!inventoryOpen)}
               className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-            >
-              Inventory
+            >Inventory
             </button>
+
+            <button
+              onClick={() => setStatsOpen(!statsOpen)}
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+            >Stats
+            </button>
+
           </div>
         </div>
       ) : null}
 
-      {inventoryOpen === true ? (
-        <div className="fixed bottom-0 flex-col w-64 h-64 gap-6 text-black bg-gray-200 left-64 position-fixed">
+      {inventoryOpen === true && playerCardVisibility === true ? (
+        <div className="fixed bottom-0 flex-col w-[256px] h-64 gap-6 text-black bg-gray-200 left-64 position-fixed">
           <p>Gold: {player.inventory.gold}</p>
           <p>Items:</p>
-          <div className="flex-col w-[95%] h-[80%] bg-gray-600 rounded-lg">
+          <div className="flex-col w-full h-full bg-gray-600 ">
             {player.inventory.items.map((item) => (
               <p
                 key={item.name}
@@ -64,16 +81,31 @@ export const PlayerCard = ({
               ${item.type === "consumable" ? "text-red-500" : ""}
               ${item.type === "weapon" ? "text-blue-500" : ""}
               ${item.type === "armor" ? "text-green-500" : ""}
-              ${item.itemlevel > 9 ? "before:content-9" : ""}
               hover:cursor-pointer
               `}
               >
-                {item.name}
+                {item.name} x {item.quantity}
               </p>
             ))}
           </div>
         </div>
       ) : null}
+
+{statsOpen === true && playerCardVisibility === true ? (
+        <div className={`fixed bottom-0 flex-col w-64 h-64 gap-6 text-black bg-gray-200 ${inventoryOpen ? "left-[512px]" : "left-[256px]"} position-fixed`}>
+          <p>Skills: </p>
+          <div className="flex-col w-full h-[fit-content] bg-gray-500">
+            {player.skills.map((skills) => (
+                <h1>{skills.name}: {skills.level}</h1>
+            ))}
+
+          </div>
+        </div>
+      ) : null}
+
+
+
+
     </div>
   );
 };
